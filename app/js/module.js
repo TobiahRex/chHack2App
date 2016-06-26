eb14a3f2bc137d0bc84eb0fa09ef44521f4431d0
RexCorpDev <rexcorpfx@gmail.com>
2016-06-25 18:10:37 -0700

    before eileen changes

--- app/js/module.js
+++ app/js/module.js
@@ -1,19 +1,20 @@
 'use strict';
 
-var app = angular.module('myApp', ['ui.router', 'angular-momentjs', 'angular-uuid', 'angular-socket-io']);
+var app = angular.module('ourApp', ['ui.router', 'angularMoment' ,'btford.socket-io']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
     .state('splash', {
-    url:'/welcome',
-    controller:'splashCtrl'
+      url         : '/welcome',
+      templateUrl : 'html/splash.html',
+      controller  : 'splashController'
     })
     .state('home', {
-      url: '/',
-      controller:'mainCtrl'
-    })
+      url         : '/home',
+      templateUrl : 'html/home.html',
+      controller  :'homeController'
+    });
 
   $urlRouterProvider.otherwise('/');
-
-})
+});
--- app/js/services.js
+++ app/js/services.js
@@ -3,20 +3,29 @@
 angular.module('ourApp')
 .service('Posts', function($http, $q) {
 
+  this.messageIntervalObj = {
+    messages  : [],
+    time      : moment().format('LTS')
+  }
+  let parentInterval = {
+    messagesPerInterval: [],
+    clicksPerInterval: [],
+    IntervalTime: moment().format('LTS')
+  };
+
+  parentInterval.messagesCount = parentInterval.messagesPerInterval.length;
+  parentInterval.clicksCount   = parentInterval.clicksPerInterval.length;
+
   // GET all
   this.getMessages = () => $http.get('/posts');
-
 
   // POST posts
   this.updatePosts = parentInterval => $http.post('/posts', parentInterval)
 
-
   // POST database
   this.createDatabase = () => $http.post('/posts?create=true');
+});
 
-})
-
-
-setTimeout(() => {
-  this.sendPosts(newMessage);
-}, 5000)
+// setTimeout(() => {
+//   this.sendPosts(newMessage);
+// }, 5000)
