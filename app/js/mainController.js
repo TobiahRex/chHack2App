60049f7ae465b93ed165b0b85f9f217aff061efc
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-06-26 19:37:21 -0700

    button behaviour

--- app/js/mainController.js
+++ app/js/mainController.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('ourApp')
-.controller('mainController', function($scope, $state, Posts, moment, ngSocket){
+.controller('mainController', function($scope, $state, Posts, moment, ngSocket, $timeout){
   console.log('maintCtrl');
 
+
+  $scope.toggleOn = true;
+
+  $scope.activateButton = (event) => {
+    if($scope.toggleOn) {
+      $scope.switchButton = true;
+    }
+    $timeout(() => {
+      $scope.switchButton = false;
+    }, 1000)
+  }
 })
